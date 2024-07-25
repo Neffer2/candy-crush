318562291310fc8f6d0433add6163d0548c73b8d
Neffer2 <n3f73r@gmail.com>
2024-07-24 22:59:52 -0500

    1. Candy Grid working

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -1,23 +1,46 @@
+const CANDY_NAMES = ['candy1', 'candy2', 'candy3', 'candy4', 'candy5'];
+
+// POSITIONS
+const CANDY_SPACING = 175;
+
 let candies = [];
 let candy;
+
 
 export class Game extends Phaser.Scene {
     constructor ()
     {
         super('Game');
     }
+    
+    init() {
+        let spacing_x = 100;
+        let spacing_y = 100;
+        let candy_count = 0;
+        while (candy_count < 20){
+            candy = this.add.sprite(spacing_x, spacing_y, CANDY_NAMES[this.getRandomInt(CANDY_NAMES.length)]).setScale(.28);
+            candies.push(candy);
+
+            spacing_x += CANDY_SPACING;
+            candy_count++;
+            
+            if (candy_count != 0 && candy_count % 4 === 0){
+                spacing_y+= CANDY_SPACING;
+                spacing_x = 100;
+                console.log(candy_count);
+            }
+        }
+    }
 
     create (){
-        for (let i = 0; i < 5; i++) {
-            candies.push(
-                candy = this.add.sprite(candy.x + 100, 100, 'candy' + (i + 1))
-                .setScale(.3)
-                .setInteractive()
-            );
-        }
+        
     } 
 
     update(){
         
+    }
+
+    getRandomInt(max){
+        return Math.floor(Math.random() * max);
     }
 }
